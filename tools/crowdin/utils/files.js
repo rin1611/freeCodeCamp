0582c0f38d1c8132a12f952814e51eb6bc7d69b3
Randell Dawson <rdawson@onepathtech.com>
2021-01-08 14:49:12 -0700

    fix: put directory functions in separate file

--- tools/crowdin/utils/files.js
+++ tools/crowdin/utils/files.js
@@ -115,89 +115,9 @@ const getFiles = async projectId => {
   return null;
 };
 
-const getDirs = async projectId => {
-  let headers = { ...authHeader };
-  let done = false;
-  let offset = 0;
-  let files = [];
-  while (!done) {
-    const endPoint = `projects/${projectId}/directories?limit=500&offset=${offset}`;
-    await delay(1000);
-    const response = await makeRequest({
-      method: 'get',
-      endPoint,
-      headers
-    });
-    if (response.data) {
-      if (response.data.length) {
-        files = [...files, ...response.data];
-        offset += 500;
-      } else {
-        done = true;
-        return files;
-      }
-    } else {
-      const { error } = response;
-      console.log(error.errorcode);
-      console.log(error.messsage);
-    }
-  }
-  return null;
-};
-
-const addDir = async (projectId, dirName, parentDirId) => {
-  let headers = { ...authHeader };
-  const endPoint = `projects/${projectId}/directories`;
-  let body = {
-    name: dirName
-  };
-  if (parentDirId) {
-    body = { ...body, directoryId: parentDirId };
-  }
-
-  const response = await makeRequest({
-    method: 'post',
-    endPoint,
-    headers,
-    body
-  });
-  return response;
-};
-
-const createDirs = async (crowdinDirs, dirPath) => {
-  // superParent is the top level directory on crowdin
-  const superParent = crowdinDirs.find(dir => !dir.data.directoryId);
-  let lastParentId = superParent.data.id;
-
-  const splitDirPath = dirPath.split('/');
-  splitDirPath.shift();
-
-  // we are assuming that the first directory in 'newFile' is the same as the superParent
-  // maybe throw a check in here to verify that's true
-
-  for (let directory of splitDirPath) {
-    const currentDirectory = crowdinDirs.find(
-      ({ data: { name, directoryId } }) => {
-        return name === directory && directoryId === lastParentId;
-      }
-    );
-
-    if (!currentDirectory) {
-      const response = await addDir(10, directory, lastParentId);
-      lastParentId = response.data.id;
-    } else {
-      lastParentId = currentDirectory.data.id;
-    }
-  }
-  return lastParentId;
-};
-
 module.exports = {
   addFile,
   updateFile,
   deleteFile,
-  getFiles,
-  addDir,
-  getDirs,
-  createDirs
+  getFiles
 };
